4a702c4fc0dfa5b02bbbc4f195efc99fdf813b46
Les Reddy <les.reddy@gmail.com>
2019-07-01 12:28:30 +0100

    initiated first piece and basic positioning

--- assets/js/tw.js
+++ assets/js/tw.js
@@ -1,5 +1,52 @@
 const canvas = document.getElementById('tetris');
 const context = canvas.getContext('2d');
 
-context.fillStyle = '#000';
-context.fillRect(0,0, canvas.width, canvas.height);
+context.scale(20,20);
+
+
+
+const matrix = [
+    [0,0,0],
+    [1,1,1],
+    [0,1,0],
+];
+
+function draw() {
+	context.fillStyle = '#000';
+	context.fillRect(0,0, canvas.width, canvas.height);
+
+	drawMatrix(player.matrix, player.pos);
+}
+
+function drawMatrix(matrix, offset) {
+	matrix.forEach((row, y) => {
+		row.forEach((value, x) => {
+			if (value !== 0) {
+				context.fillStyle = 'red';
+				context.fillRect(x + offset.x,
+					y + offset.y,
+					1, 1);
+			}
+		})
+	})
+}
+
+// This function clears the canvas so that it can re-draw the shape
+
+let lastTime = 0;
+
+function update(time = 0) {
+	const deltaTime = time - lastTime;
+	lastTime = time;
+	console.log(deltaTime);
+	draw();
+	requestAnimationFrame(update);
+
+}
+
+const player = {
+	pos: {x: 5, y: 5},
+	matrix: matrix,
+}
+
+update();

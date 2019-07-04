eaf34c70b064fa1deef9ecd85feef33c07bc42ed
Les Reddy <les.reddy@gmail.com>
2019-07-04 06:55:00 +0100

    first piece rotating & dropping correctly in arena

--- assets/js/tw.js
+++ assets/js/tw.js
@@ -93,6 +93,45 @@ function playerMove(dir) {
 	}
 }
 
+//below function implements the player rotate whilst ensure it rotates within arena
+
+function playerRotate(dir) {
+	const pos = player.pos.x;
+	let offset = 1;
+	rotate(player.matrix, dir);
+	while (collide(arena, player)) {
+		player.pos.x += offset;
+		offset = -(offset + (offset > 0 ? 1 : -1));
+		if (offset > player.matrix[0].length) {
+			rotate(player.matrix, -dir);
+			player.pos.x = pos;
+			return;
+		}
+	}
+}
+
+
+//below function switches the pieces position by rotating the matrix
+
+function rotate(matrix, dir) {
+	for (let y = 0; y < matrix.length; ++y) {
+		for (let x = 0; x < y; ++x) {
+			[
+				matrix[x][y],
+				matrix[y][x],
+			] = [
+				matrix[y][x],
+				matrix[x][y],
+			];
+		}
+	}
+	if (dir > 0) {
+		matrix.forEach(row => row.reverse());
+	} else {
+		matrix.reverse();
+	}
+}
+
 let dropCounter = 0;
 let dropInterval = 1000;
 
@@ -133,6 +172,10 @@ document.addEventListener('keydown', event => {
 		playerMove(1);
 	} else if (event.keyCode === 40) {
 	playerDrop();
+	} else if (event.keyCode === 38) {
+		playerRotate(-1);
+	} else if (event.keyCode === 87) {
+		playerRotate(1);
 	}
 });
 
